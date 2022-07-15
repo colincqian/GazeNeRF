d532de3ab0c12d0dae8bbb4adf645926f537d09c
colincqian <qianchanglin21@outlook.com>
2022-07-15 12:27:53 +0200

    add control eye gaze shell script and .py

--- Utils/RenderUtils.py
+++ Utils/RenderUtils.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 from HeadNeRFOptions import BaseOptions
 import json
 import numpy as np
+import cv2
 import math
 
 
@@ -108,10 +109,10 @@ class RenderUtils(object):
                 pred_dict = net("test",batch_xy, batch_uv, **code_info,**cam_info)
             coarse_fg_rgb = pred_dict["coarse_dict"]["merge_img"]
             coarse_fg_rgb = (coarse_fg_rgb[0].detach().cpu().permute(1, 2, 0).numpy()* 255).astype(np.uint8)
+            coarse_fg_rgb = cv2.cvtColor(coarse_fg_rgb, cv2.COLOR_BGR2RGB)
             res_img_list.append(coarse_fg_rgb)
 
         return res_img_list
-    
     
     def render_morphing_res(self, net, code_info_1, code_info_2, nums):
         
@@ -136,6 +137,36 @@ class RenderUtils(object):
                 pred_dict = net("test",batch_xy, batch_uv, **code_info,**self.base_cam_info)
             coarse_fg_rgb = pred_dict["coarse_dict"]["merge_img"]
             coarse_fg_rgb = (coarse_fg_rgb[0].detach().cpu().permute(1, 2, 0).numpy()* 255).astype(np.uint8)
+            res_img_list.append(coarse_fg_rgb)
+            
+        return res_img_list
+
+    def render_gaze_redirect_res(self, net, code_info_1, code_info_2, nums, gaze_dim):
+        ##code1 and code2 only have difference in last few columns (gaze tensor)
+        batch_xy = self.ray_xy
+        batch_uv = self.ray_uv
+        res_img_list = []
+        shape_code = code_info_1["shape_code"]
+        appea_code = code_info_1["appea_code"]
+        
+        loop_bar = tqdm(range(nums), leave=True)
+        for i in loop_bar:
+            loop_bar.set_description("Generate Morphing Res")
+            tv = 1.0 - (i / (nums - 1))
+            shape_code = code_info_1["shape_code"]* tv + code_info_2["shape_code"] * (1 - tv)
+            appea_code = code_info_1["appea_code"]* tv + code_info_2["appea_code"] * (1 - tv)
+            
+            code_info = {
+                "bg_code":None,
+                "shape_code":shape_code, 
+                "appea_code":appea_code
+            }
+            
+            with torch.set_grad_enabled(False):
+                pred_dict = net("test",batch_xy, batch_uv, **code_info,**self.base_cam_info)
+            coarse_fg_rgb = pred_dict["coarse_dict"]["merge_img"]
+            coarse_fg_rgb = (coarse_fg_rgb[0].detach().cpu().permute(1, 2, 0).numpy()* 255).astype(np.uint8)
+            coarse_fg_rgb = cv2.cvtColor(coarse_fg_rgb, cv2.COLOR_BGR2RGB)
             res_img_list.append(coarse_fg_rgb)
             
         return res_img_list
